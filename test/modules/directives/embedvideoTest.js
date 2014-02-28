732f5e2bc9ab2be789398fc3402ab18a84d5c0ed
erost <erost@erost.net>
2014-02-28 17:45:59 -0500

    updated test cases to match the new template

--- test/modules/directives/embedvideoTest.js
+++ test/modules/directives/embedvideoTest.js
@@ -23,17 +23,18 @@ describe('embedVideo', function() {
 	describe('embed youtube', function() {
 		it('should embed a youtube video', inject(function () {
 			inject(function ($compile) {
-				var element;
-				element = $compile('<a href="http://www.youtube.com/watch?v=LOKyEt36Kjc" embed-video controls=0 >Watch</a>')(scope);
+				var rootElement;
+				rootElement = $compile('<a ng-href="http://www.youtube.com/watch?v=LOKyEt36Kjc" embed-video controls=0 >Watch</a>')(scope);
 				scope.$apply();
-				var elementData = getURLandOptions(element.attr('src'));
+				var element = rootElement[0].firstChild;
+				expect(element).toBeDefined();
+				expect(element.nodeName.toLowerCase()).toEqual('iframe');
+				var elementData = getURLandOptions(element.getAttribute('src'));
 				expect(elementData.url).toEqual('http://www.youtube.com/embed/LOKyEt36Kjc');
 				expect(Object.keys(elementData.options).length).toEqual(3);
 				expect(elementData.options.loop).toEqual('0');
 				expect(elementData.options.controls).toEqual('0');
 				expect(elementData.options.autoplay).toEqual('0');
-				expect(element.attr('width')).toEqual('560');
-				expect(element.attr('height')).toEqual('315');
 			});
 		}))
 	})
@@ -41,17 +42,18 @@ describe('embedVideo', function() {
 	describe('embed https youtube', function() {
 		it('should embed a youtube video using https protocol', inject(function () {
 			inject(function ($compile) {
-				var element;
-				element = $compile('<a href="https://www.youtube.com/watch?v=LOKyEt36Kjc" embed-video controls=0 >Watch</a>')(scope);
+				var rootElement;
+				rootElement = $compile('<a ng-href="https://www.youtube.com/watch?v=LOKyEt36Kjc" embed-video controls=0 >Watch</a>')(scope);
 				scope.$apply();
-				var elementData = getURLandOptions(element.attr('src'));
+				var element = rootElement[0].firstChild;
+				expect(element).toBeDefined();
+				expect(element.nodeName.toLowerCase()).toEqual('iframe');
+				var elementData = getURLandOptions(element.getAttribute('src'));
 				expect(elementData.url).toEqual('https://www.youtube.com/embed/LOKyEt36Kjc');
 				expect(Object.keys(elementData.options).length).toEqual(3);
 				expect(elementData.options.loop).toEqual('0');
 				expect(elementData.options.controls).toEqual('0');
 				expect(elementData.options.autoplay).toEqual('0');
-				expect(element.attr('width')).toEqual('560');
-				expect(element.attr('height')).toEqual('315');
 			});
 		}))
 	})
@@ -59,17 +61,20 @@ describe('embedVideo', function() {
 	describe('embed dailymotion', function() {
 		it('should embed a dailymotion video', inject(function () {
 			inject(function ($compile) {
-				var element;
-				element = $compile('<a href="http://www.dailymotion.com/video/xxd68z" embed-video force-quality=hq width=1280 height=720>Watch</a>')(scope);
+				var rootElement;
+				rootElement = $compile('<a ng-href="http://www.dailymotion.com/video/xxd68z" embed-video force-quality=hq width=1280 height=720>Watch</a>')(scope);
 				scope.$apply();
-				var elementData = getURLandOptions(element.attr('src'));
+				var element = rootElement[0].firstChild;
+				expect(element).toBeDefined();
+				expect(element.nodeName.toLowerCase()).toEqual('iframe');
+				var elementData = getURLandOptions(element.getAttribute('src'));
 				expect(elementData.url).toEqual('http://www.dailymotion.com/embed/video/xxd68z');
 				expect(Object.keys(elementData.options).length).toEqual(3);
 				expect(elementData.options.forceQuality).toEqual('hq');
 				expect(elementData.options.logo).toEqual('0');
 				expect(elementData.options.autoPlay).toEqual('0');
-				expect(element.attr('width')).toEqual('1280');
-				expect(element.attr('height')).toEqual('720');
+				expect(element.getAttribute('width')).toEqual('1280');
+				expect(element.getAttribute('height')).toEqual('720');
 			});
 		}))
 	})
@@ -77,17 +82,18 @@ describe('embedVideo', function() {
 	describe('embed vimeo', function() {
 		it('should embed a vimeo video', inject(function () {
 			inject(function ($compile) {
-				var element;
-				element = $compile('<a href="http://vimeo.com/53953" embed-video color=c9ff23 >Watch</a>')(scope);
+				var rootElement;
+				rootElement = $compile('<a ng-href="http://vimeo.com/53953" embed-video color=c9ff23 >Watch</a>')(scope);
 				scope.$apply();
-				var elementData = getURLandOptions(element.attr('src'));
+				var element = rootElement[0].firstChild;
+				expect(element).toBeDefined();
+				expect(element.nodeName.toLowerCase()).toEqual('iframe');
+				var elementData = getURLandOptions(element.getAttribute('src'));
 				expect(elementData.url).toEqual('http://player.vimeo.com/video/53953');
 				expect(Object.keys(elementData.options).length).toEqual(3);
 				expect(elementData.options.color).toEqual('c9ff23');
 				expect(elementData.options.loop).toEqual('0');
 				expect(elementData.options.autoplay).toEqual('0');
-				expect(element.attr('width')).toEqual('560');
-				expect(element.attr('height')).toEqual('315');
 			});
 		}))
 	})
