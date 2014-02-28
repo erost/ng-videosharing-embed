76c6e8216b920a0f1892ff4962ac2e72aaa1700f
erost <erost@erost.net>
2014-02-28 13:23:56 -0500

    proper 0.1.9 version

--- build/ng-videosharing-embed.js
+++ build/ng-videosharing-embed.js
@@ -58,7 +58,7 @@ angular.module('videosharing-embed').factory('RegisteredPlayers', [ 'PlayerConfi
             whitelist: ['autoplay', 'color', 'loop'],
             playerID: 'player.vimeo.com/video/',
             protocol: 'http://',
-            playerRegExp: /(http:\/\/)(vimeo\.com)\/([A-Za-z0-9]+)/
+            playerRegExp: /(http:\/\/)vimeo\.com\/([A-Za-z0-9]+)/
         },
         dailymotion: {
             options: {
@@ -68,7 +68,7 @@ angular.module('videosharing-embed').factory('RegisteredPlayers', [ 'PlayerConfi
             whitelist: ['autoPlay', 'logo', 'forceQuality'],
             playerID: 'www.dailymotion.com/embed/video/',
             protocol: 'http://',
-            playerRegExp: /(http:\/\/)(www\.dailymotion\.com)\/video\/([A-Za-z0-9]+)/
+            playerRegExp: /(http:\/\/)www\.dailymotion\.com\/video\/([A-Za-z0-9]+)/
         }
     };
     var players = [];
