2a3a5385a4380829ff616aaf51190348da340bd1
erost <erost@erost.net>
2014-03-02 09:38:59 -0500

    use PhantomJS everywhere

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     // CLI --browsers Chrome,Firefox,Safari
-    browsers: [process.env.TRAVIS ? 'Firefox' : 'PhantomJS'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     // CLI --capture-timeout 5000
