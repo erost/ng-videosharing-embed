55fff2ee7fa1ae1450e126f58ffb47e69f4b93e5
Thomas Devol <pair+thomas@rally.org>
2014-04-07 15:37:17 -0700

    Replace element instead of appending to fix IE issues

--- src/directives/embedvideo.js
+++ src/directives/embedvideo.js
@@ -3,6 +3,7 @@ angular.module('videosharing-embed').directive('embedVideo', [ '$filter' , 'Regi
     return {
         restrict: "A",
         template: '<iframe width="{{width}}" height="{{height}}" data-ng-src="{{trustedVideoSrc}}" frameborder="0"></iframe>',
+        replace: true,
         scope : {
             height: '@height',
             width: '@width'
@@ -10,37 +11,33 @@ angular.module('videosharing-embed').directive('embedVideo', [ '$filter' , 'Regi
         link: function ($scope, $element, $attrs) {
             //handle the use of both ng-href and href
             $attrs.$observe('href', function(url) {
-				if(url === undefined)
-					return;
-				var player = null;
-            
-				//search for the right player in the list of registered players
-				angular.forEach(RegisteredPlayers, function (value) {
-					if (value.isPlayerFromURL(url)) {
-						player = value;
-					}
-				});
-				if(player === null)
-				    return; //haven't found a match for a valid registered player
-				
-				//get the videoID
-				var videoID = url.match(player.playerRegExp)[2];
+              if(url === undefined)
+                return;
+              var player = null;
+              //search for the right player in the list of registered players
+              angular.forEach(RegisteredPlayers, function (value) {
+                if (value.isPlayerFromURL(url)) {
+                  player = value;
+                }
+              });
+              if(player === null)
+                  return; //haven't found a match for a valid registered player
+              //get the videoID
+              var videoID = url.match(player.playerRegExp)[2];
 
-				//copy configuration from player
-				var config = player.config;
-            
-				//get the protocol
-				var protocol = url.match(player.playerRegExp)[1] || '';
+              //copy configuration from player
+              var config = player.config;
+              //get the protocol
+              var protocol = url.match(player.playerRegExp)[1] || '';
 
-				//overwrite playback options
-				angular.forEach($filter('whitelist')($attrs, player.whitelist), function (value, key) {
-					config.options[key] = value;
-				});
-			
-				//build and trust the video URL
-				var untrustedVideoSrc = protocol + '//' + config.playerID + videoID + $filter('videoOptions')(config.options);
-				$scope.trustedVideoSrc = $sce.trustAsResourceUrl(untrustedVideoSrc);
-			});
+              //overwrite playback options
+              angular.forEach($filter('whitelist')($attrs, player.whitelist), function (value, key) {
+                config.options[key] = value;
+              });
+              //build and trust the video URL
+              var untrustedVideoSrc = protocol + '//' + config.playerID + videoID + $filter('videoOptions')(config.options);
+              $scope.trustedVideoSrc = $sce.trustAsResourceUrl(untrustedVideoSrc);
+            });
         }
     }
 }]);
